4433907260a3bcb015f20b62d3c873d5afa2b1d3
Yeonggyun Kim <kygha7205@naver.com>
2021-08-10 15:11:30 +0900

    Add missing npm script

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     reactRefresh(),
-    viteCompression({ algorithm: 'gzip' }),
+    viteCompression({ algorithm: 'brotliCompress' }),
     VitePWA({
       includeAssets: [
         'favicon.svg',
