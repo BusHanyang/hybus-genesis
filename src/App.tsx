9b307d3949d3aff2de42db551156d0b6493fbd0e
Yusun <solarpy@hanyang.ac.kr>
2022-05-31 17:56:26 +0900

    Add full time table layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,64 @@
 import './App.css'
+import 'styled-components'
 
 import React, { useState } from 'react'
-import { useRegisterSW } from 'virtual:pwa-register/react'
 
-import { Card } from './app/components'
 import logo from './logo.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const {
-    offlineReady: [offlineReady, setOfflineReady],
-    needRefresh: [needRefresh, setNeedRefresh],
-    updateServiceWorker,
-  } = useRegisterSW({
-    onRegistered(r: any) {
-      // eslint-disable-next-line prefer-template
-      console.log('SW Registered: ' + r)
-    },
-    onRegisterError(error: any) {
-      console.log('SW registration error', error)
-    },
-  })
+  const [table, changeFullTable] = useState<boolean>(false)
 
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        {' '}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-      <Card />
+      </div>
+      <button
+        className="full-button"
+        onClick={() => {
+          changeFullTable(!table)
+        }}
+      >
+        한대앞 전체시간표
+      </button>
+      {table === true ? <FullTime></FullTime> : null}
+    </div>
+  )
+}
+
+function FullTime() {
+  return (
+    <div>
+      <div>
+        <p>**버스 정류장</p>
+      </div>
+      <div className="horizon-line"></div>
+      <div>
+        <p>시기</p>
+      </div>
+      <div className="horizon-line"></div>
+
+      <div>
+        <p>요일</p>
+      </div>
+      <div className="horizon-line"></div>
+      <div className="time-box">
+        <div>8시</div>
+        <div>
+          <div className="circle">순환</div>
+          <div></div>
+          <div className="direct">직행</div>
+        </div>
+      </div>
+      <div className="time-box">
+        <p>9시</p>
+        <div>순환</div>
+      </div>
+      <div className="time-box">
+        <p>10시</p>
+        <div>순환</div>
+      </div>
     </div>
   )
 }
