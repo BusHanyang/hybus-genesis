1c9022adb8e21dba57a3417c30a06e3e87d1cfca
HyeongChang Lee <hc9904@hanyang.ac.kr>
2022-06-01 11:56:43 +0900

    restart project

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import {RecoilRoot} from 'recoil'
+
+import App from './App'
+
+const queryClient = new QueryClient()
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    reactRefresh(),
+    viteCompression({ algorithm: 'brotliCompress' }),
+    VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'safari-pinned-tab.svg',
+      ],
+      manifest: {
+        theme_color: '#0e4a84',
+        background_color: '#898c8e',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        name: 'HYBUS',
+        short_name: 'HYBUS',
+        icons: [
+          {
+            src: 'image/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'image/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  publicDir: './public',
+})
