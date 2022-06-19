d0bb2a8cced6c2aed0201195783c9465bc9b49d3
Taewan Park <swimingtw@gmail.com>
2022-06-19 22:38:36 +0900

    Apply tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'chip-red': '#FF897A',
+        'chip-blue': '#7FAAFF',
+      },
+    },
+  },
+  plugins: [],
+}
