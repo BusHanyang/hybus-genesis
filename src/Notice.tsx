048599dad01cd6d642b93fb32f1dae5f233756e7
YusunPark <solarpy@hanyang.ac.kr>
2022-07-01 15:59:25 +0900

    Add function for link

--- src/Notice.tsx
+++ src/Notice.tsx
@@ -2,9 +2,19 @@ import './Notice.css'
 
 import React, { useEffect, useState } from 'react'
 
-function Notice() {
+const Notice = () => {
+  const url: string[] = [
+    'https://huchu.link/yiBacVJ',
+    'https://huchu.link/0XaPgDF',
+  ]
   return (
-    <div id="notice" className="card">
+    <div
+      id="notice"
+      className="card"
+      onClick={() => {
+        window.location.href = url[0]
+      }}
+    >
       <p id="gong">공지</p>
       <p id="notice_text">2022-여름학기 반영완료</p>
       <p id="notice_date">06/19</p>
