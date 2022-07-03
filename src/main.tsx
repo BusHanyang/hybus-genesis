1b7f3db777b67f27ab05a498bca84c887481c0a5
Taewan Park <swimingtw@gmail.com>
2022-07-03 11:15:30 +0900

    Refactor code with eslint

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import {RecoilRoot} from 'recoil'
+import { RecoilRoot } from 'recoil'
 
 import App from './App'
 
 const queryClient = new QueryClient()
 
-
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} />
       <RecoilRoot>
         <App />
       </RecoilRoot>
