ab44b04341033b841538614e3fe399df7248c2a7
YusunPark <solarpy@hanyang.ac.kr>
2022-07-08 13:53:15 +0900

    Add carousel

--- src/Notice.tsx
+++ src/Notice.tsx
@@ -8,17 +8,43 @@ const Notice = () => {
     'https://huchu.link/0XaPgDF',
   ]
   return (
-    <a
-      id="notice"
-      className="card"
-      onClick={() => {
-        window.open(url[0])
-      }}
-    >
-      <p id="gong">공지</p>
-      <p id="notice_text">2022-여름학기 반영완료</p>
-      <p id="notice_date">06/19</p>
-    </a>
+    <div>
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide relative"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner relative w-full overflow-hidden">
+          <div className="carousel-item active relative float-left w-full">
+            <a
+              id="notice"
+              className="card"
+              onClick={() => {
+                window.open(url[0])
+              }}
+            >
+              <p id="gong">공지</p>
+              <p id="notice_text">2022-여름학기 반영완료</p>
+              <p id="notice_date">06/19</p>
+            </a>
+          </div>
+          <div className="carousel-item relative float-left w-full">
+            <a
+              id="notice"
+              className="card"
+              onClick={() => {
+                window.open(url[0])
+              }}
+            >
+              <p id="gong">공지</p>
+              <p id="notice_text">2022-여름학기 반영완료</p>
+              <p id="notice_date">06/19</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
+    </div>
   )
 }
 
