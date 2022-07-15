2bc2da67a77119413319635062f89d91cac46886
YusunPark <solarpy@hanyang.ac.kr>
2022-07-15 17:37:46 +0900

    Change method of loading data to api

--- src/Notice.tsx
+++ src/Notice.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 type Info = {
@@ -8,45 +9,59 @@ type Info = {
 }
 
 const Notice = () => {
-  const info: Info[] = [
-    {
-      label: '공지',
-      title: 'testtesttesttes',
-      date: '07/23',
-      url: 'https://huchu.link/yiBacVJ',
-    },
-    {
-      label: '공지',
-      title: 'testtest231',
-      date: '07/23',
-      url: 'https://huchu.link/0XaPgDF',
-    },
-  ]
+  const [data, setData] = useState<Array<Info>>([])
+  const [isLoaded, setLoaded] = useState<boolean>(false)
+
+  const getData = async (): Promise<Array<Info>> => {
+    return await axios
+      .get(
+        'https://proxy.anoldstory.workers.dev/https://announcements.hybus.app/'
+      )
+      .then((res) => {
+        return res.data
+      })
+      .catch((err) => {
+        console.log(err)
+        return new Array<Info>()
+      })
+      .then((res) => res as Array<Info>)
+  }
+
+  useEffect(() => {
+    if (!isLoaded) {
+      getData().then((res) => {
+        setData(res)
+        setLoaded(true)
+      })
+    }
+  }, [])
 
   const [num, changeNum] = useState<number>(0)
 
   useEffect(() => {
     setTimeout(() => {
-      changeNum((num + 1) % info.length)
+      changeNum((num + 1) % data.length)
     }, 3000)
-  }, [num])
+  }, [data.length, num])
 
   return (
     <div>
       <div className=" relative w-full overflow-hidden">
-        {info.map((item, idx) => {
-          return (
-            <div key={idx} className={idx === num ? '' : 'hidden'}>
-              <Box
-                label={item.label}
-                title={item.title}
-                date={item.date}
-                url={item.url}
-              />
-            </div>
-          )
-        })}
-      </div>
+        <div className="w-full card">
+          {data.map((item, idx) => {
+            return (
+              <div key={idx} className={idx === num ? '' : 'hidden'}>
+                <Box
+                  label={item.label}
+                  title={item.title}
+                  date={item.date}
+                  url={item.url}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>{' '}
     </div>
   )
 }
@@ -58,20 +73,18 @@ const Box = (props: {
   date: string
 }) => {
   return (
-    <div className=" w-full card">
-      <a
-        className="animate-carousel cursor-pointer"
-        onClick={() => {
-          window.open(props.url)
-        }}
-      >
-        <p className="float-left font-bold text-base text-chip-red">
-          {props.label}
-        </p>
-        <p className="float-left px-3 font-medium text-base">{props.title}</p>
-        <p className="float-right px-3 font-normal text-base">{props.date}</p>
-      </a>
-    </div>
+    <a
+      className="animate-carousel cursor-pointer "
+      onClick={() => {
+        window.open(props.url)
+      }}
+    >
+      <p className="float-left font-bold text-base text-chip-red">
+        {props.label}
+      </p>
+      <p className="float-left px-3 font-medium text-base">{props.title}</p>
+      <p className="float-right px-3 font-normal text-base">{props.date}</p>
+    </a>
   )
 }
 
