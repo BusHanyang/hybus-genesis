020935dd3fa6a62731c989ff6722f886b8021f99
kcyoow <46476820+kcyoow@users.noreply.github.com>
2022-08-29 15:04:05 +0000

    Merge refreshing-content & refreshing-contentCSS

--- src/app/components/ptr/refreshing-content.tsx
+++ src/app/components/ptr/refreshing-content.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
-import { LdsEllipsis, LdsEllipsisDiv } from './refreshing-contentCSS'
+import styled from 'styled-components'
+import tw from 'twin.macro'
 
 // Source: https://loading.io/css/
+
+const LdsEllipsis = styled.div`
+  ${tw`inline-block relative w-16 h-16`}
+`
+
+const LdsEllipsisDiv = styled.div<{ theme: string }>`
+  ${({ theme }) => {
+    return theme === 'dark'
+      ? tw`absolute top-7 w-3 h-3 rounded-full bg-white ease-ptrTran`
+      : tw`absolute top-7 w-3 h-3 rounded-full bg-gray-700 ease-ptrTran`
+  }}
+  &:nth-child(1) {
+    ${tw`left-1.5 animate-ldsEllipsis1`}
+  }
+  &:nth-child(2) {
+    ${tw`left-1.5 animate-ldsEllipsis2`}
+  }
+  &:nth-child(3) {
+    ${tw`left-7 animate-ldsEllipsis2`}
+  }
+  &:nth-child(4) {
+    ${tw`left-11 animate-ldsEllipsis3`}
+  }
+`
+
+
 
 interface RefreshingContentProps {
   mode: string
