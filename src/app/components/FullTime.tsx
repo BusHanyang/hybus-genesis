4a04b4374f7a83d01efe184f007562aeb965651b
YusunPark <solarpy@hanyang.ac.kr>
2022-09-11 11:38:57 +0900

    Fix line height size

--- src/app/components/FullTime.tsx
+++ src/app/components/FullTime.tsx
@@ -99,21 +99,21 @@ const TimeBox = (props: FilteredTimeTables) => {
   const { t } = useTranslation()
   return (
     <>
-      <div className="h-24 bg-[#E1E2EC] dark:bg-[#44464E] rounded-xl drop-shadow-lg grid grid-cols-6 p-5 hm:h-20 hm:p-3 hm:text-sm">
+      <div className="h-24 bg-[#E1E2EC] dark:bg-[#44464E] rounded-xl drop-shadow-lg grid grid-cols-6 p-5 hm:h-20 hm:p-2.5 hm:text-sm">
         <div className="font-bold self-center">
           {props.time}
           {t('o_clock')}
         </div>
         <div className="font-medium inline-grid grid-flow-row gap-2 col-span-5 hm:gap-px">
           <div
-            className={`inline-grid grid-cols-5 ${
+            className={`inline-grid grid-cols-5  ${
               props.circle.length === 0 ? 'hidden' : 'block'
             }`}
           >
             <div className="self-center bg-chip-red h-fit  dark:text-black py-1 w-12 rounded-full inline-block text-center hm:w-10 hm:py-0.5">
               {t('cycle')}
             </div>
-            <div className="self-center text-left ml-3 col-span-4">
+            <div className="self-center text-left ml-3 col-span-4 ">
               {props.circle.join(' ')}
             </div>
           </div>
@@ -125,7 +125,7 @@ const TimeBox = (props: FilteredTimeTables) => {
             <div className="self-center bg-chip-blue h-fit  dark:text-black py-1 w-12 rounded-full inline-block text-center  hm:w-10 hm:py-0.5">
               {t('direct')}
             </div>
-            <div className="self-center text-left ml-3 col-span-4">
+            <div className="self-center text-left ml-3 col-span-4 hm:leading-none">
               {props.direct.map((time, idx) => {
                 return (
                   <React.Fragment key={idx}>
