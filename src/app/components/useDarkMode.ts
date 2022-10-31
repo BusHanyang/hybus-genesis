83c53968ca10e4a0d5ca20b092bf4832c08b467f
Yeonggyun Kim <kygha7205@naver.com>
2022-11-01 01:15:02 +0900

    Merge dark theme manifest generate script into react code.

--- src/app/components/useDarkMode.ts
+++ src/app/components/useDarkMode.ts
@@ -4,16 +4,23 @@ import { THEME, useDarkmodeContext } from '../context/ThemeContext'
 
 export const useDarkMode = () => {
   const { setTheme, theme } = useDarkmodeContext()
+  const BAR_STYLE = document.querySelector('meta[name=theme-color]')
   const toggleTheme = useCallback(() => {
     if (theme === THEME.LIGHT) {
+      if (BAR_STYLE) BAR_STYLE.setAttribute('content', '#27272A')
+      document.body.classList.add('dark')
+      document.body.style.backgroundColor = '#27272A'
       window.localStorage.setItem('theme', THEME.DARK)
       setTheme(THEME.DARK)
     } else {
+      if (BAR_STYLE) BAR_STYLE.setAttribute('content', '#FFFFFF')
+      document.body.classList.remove('dark')
+      document.body.style.backgroundColor = '#FFFFFF'
       window.localStorage.setItem('theme', THEME.LIGHT)
       setTheme(THEME.LIGHT)
     }
     // location.reload()
-  }, [setTheme, theme])
+  }, [BAR_STYLE, setTheme, theme])
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem('theme')
