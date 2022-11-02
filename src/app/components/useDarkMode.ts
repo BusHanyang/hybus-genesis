822f932220386b1f52d633ed537370560d9db578
Taewan Park <swimingtw@gmail.com>
2022-11-02 17:49:15 +0900

    Fix status bar color mismatch when switching themes

--- src/app/components/useDarkMode.ts
+++ src/app/components/useDarkMode.ts
@@ -5,6 +5,25 @@ import { THEME, useDarkmodeContext } from '../context/ThemeContext'
 export const useDarkMode = () => {
   const { setTheme, theme } = useDarkmodeContext()
   const BAR_STYLE = document.querySelector('meta[name=theme-color]')
+
+  const setBarStyle = useCallback(
+    (color: string) => {
+      BAR_STYLE?.setAttribute('content', color)
+    },
+    [BAR_STYLE]
+  )
+
+  const setBackground = useCallback(() => {
+    if (BAR_STYLE)
+      theme === THEME.DARK ? setBarStyle('#27272A') : setBarStyle('#FFFFFF')
+
+    if (theme === THEME.DARK) {
+      document.body.style.backgroundColor = '#27272A'
+    } else {
+      document.body.style.backgroundColor = '#FFFFFF'
+    }
+  }, [BAR_STYLE, setBarStyle, theme])
+
   const toggleTheme = useCallback(() => {
     if (theme === THEME.LIGHT) {
       if (BAR_STYLE) BAR_STYLE.setAttribute('content', '#27272A')
@@ -26,8 +45,9 @@ export const useDarkMode = () => {
     const localTheme = window.localStorage.getItem('theme')
     if (localTheme) {
       setTheme(localTheme as THEME)
+      setBackground()
     }
-  }, [setTheme])
+  }, [setBackground, setTheme])
 
   return { toggleTheme }
 }
