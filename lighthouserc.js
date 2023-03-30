656749a89afededa0101afe82778c2225290e839
Taewan Park <swimingtw@gmail.com>
2023-03-31 01:17:00 +0900

    Add Lighthouse CI (#69)
    * Add lighthouse CI Support

---
+++ lighthouserc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './dist',
+      url: 'http://localhost/index.html',
+      numberOfRuns: 5,
+      settings: {
+        skipAudits: [
+          'redirects-http',
+          'is-on-https',
+          'works-offline',
+          'bf-cache',
+          'csp-xss',
+          'uses-rel-preconnect',
+          'color-contrast',
+          'unused-css-rules',
+          'unused-javascript',
+        ],
+      },
+    },
+    upload: {
+      target: 'lhci',
+      serverBaseUrl: 'https://lighthouse.hybus.app',
+      token: process.env.LHCI_TOKEN,
+      githubAppToken: process.env.LHCI_GITHUB_APP_TOKEN,
+      basicAuth: {
+        username: process.env.LHCI_USERNAME,
+        password: process.env.LHCI_PASSWORD,
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
+    },
+  },
+}
