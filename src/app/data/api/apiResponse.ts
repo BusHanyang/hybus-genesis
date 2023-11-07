709d084af9fdba777c2a9ba3073fa72644dd48dd
Taewan Park <swimingtw@gmail.com>
2023-11-08 01:14:02 +0900

    Change response type to actual data

--- src/app/data/api/apiResponse.ts
+++
@@ -1,6 +0,0 @@
-import { ResponseStatus } from '@/data'
-
-export interface APIResponse<T> {
-  status: ResponseStatus
-  data: T | null
-}
--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -1,5 +1,3 @@
-export type { APIResponse } from './api/apiResponse'
-export type { ResponseStatus } from './api/responseStatus'
 export type { StopLocation } from './common/stopLocation'
 export type { Changelog } from './modal/changelog'
 export type { NoticeInfo } from './notice/noticeInfo'
