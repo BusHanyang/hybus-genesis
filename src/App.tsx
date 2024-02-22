f6ac690924b7ce00d9bab4145172f5124c5ea563
Taewan Park <swimingtw@gmail.com>
2024-02-22 12:50:43 +0900

    Change copyright year to 2024

--- src/App.tsx
+++ src/App.tsx
@@ -390,7 +390,7 @@ function App() {
                         id="copyright"
                         className="dark:text-white pt-3 hsm:text-sm hsm:leading-4"
                       >
-                        Copyright © 2020-2023{' '}
+                        Copyright © 2020-2024{' '}
                         <a
                           className="underline"
                           target="_blank"
