3a720072936ace1bd1e94ee0f7afa44b89e0be4c
Taewan Park <swimingtw@gmail.com>
2024-05-04 02:50:02 +0900

    Optimize modal style component

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -11,7 +11,11 @@ const ModalBackground = styled.div<{ $isopen: boolean }>`
     return $isopen ? tw`flex items-center` : null
   }}
 `
-const ModalMain = styled(ModalBackground)<{ $isopen: boolean; $isani: boolean }>`
+
+const ModalMain = styled(ModalBackground)<{
+  $isopen: boolean
+  $isani: boolean
+}>`
   ${({ $isopen }) => {
     return $isopen ? tw`flex items-center animate-modalBgShow` : null
   }}
@@ -19,39 +23,44 @@ const ModalMain = styled(ModalBackground)<{ $isopen: boolean; $isani: boolean }>
     return $isani ? tw`animate-modalBgClose` : null
   }}
 `
+
 const ModalButton = styled.button`
   ${tw`outline-none cursor-pointer border-0`}
 `
+
 const ModalSection = styled.section<{ $isani: boolean }>`
   ${tw`w-11/12 max-w-screen-sm mx-auto rounded-lg bg-white overflow-auto animate-modalShow`}
   ${({ $isani }) => {
     return $isani ? tw`animate-modalClose` : null
   }}
 `
+
 const ModalHeader = styled.header<{ theme: string }>`
+  ${tw`relative pt-4 pr-16 pb-4 pl-4 font-bold font-Ptd`}
   ${({ theme }) => {
-    return theme === 'dark'
-      ? tw`relative pt-4 pr-16 pb-4 pl-4 bg-zinc-800 font-bold text-white font-Ptd`
-      : tw`relative pt-4 pr-16 pb-4 pl-4 bg-white font-bold font-Ptd`
+    return theme === 'dark' ? tw`bg-zinc-800 text-white` : tw`bg-white`
   }}
 `
+
 const ModalFooterButton = styled(ModalButton)`
   ${tw`py-2 px-4 text-white bg-gray-500 font-Ptd rounded-md text-xs`}
 `
 
 const ModalSubMain = styled.main<{ theme: string }>`
+  ${tw`p-4 border-y border-solid`}
   ${({ theme }) => {
     return theme === 'dark'
-      ? tw`p-4 border-y border-solid border-zinc-800 bg-gray-700 text-white`
-      : tw`p-4 border-y border-solid border-sky-50`
+      ? tw`border-zinc-800 bg-gray-700 text-white`
+      : tw`border-sky-50`
   }}
 `
 
 const ModalFooter = styled.footer<{ theme: string }>`
+  ${tw`py-3 px-4 text-right`}
   ${({ theme }) => {
-    return theme === 'dark'
-      ? tw`py-3 px-4 text-right text-white bg-gray-700`
-      : tw`py-3 px-4 text-right`
+    if (theme === 'dark') {
+      return tw`text-white bg-gray-700`
+    }
   }}
 `
 
@@ -75,7 +84,11 @@ export const Modal = (props: {
 
   return (
     <ModalBackground $isopen={props.open} onClick={handleClickModalBackground}>
-      <ModalMain $isopen={props.open} $isani={props.ani} ref={modalBackgroundRef}>
+      <ModalMain
+        $isopen={props.open}
+        $isani={props.ani}
+        ref={modalBackgroundRef}
+      >
         {props.open ? (
           <ModalSection $isani={props.ani}>
             {props.mTarget === 'Fabs' ? (
